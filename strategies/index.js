289c30988019a60bbe787c63a6b8304c7bef8c8e
Thomas Rayden <thomasmwrayden@gmail.com>
2018-10-02 13:20:33 +0800

    fix strategy module

--- strategies/index.js
+++ strategies/index.js
@@ -1,9 +1,5 @@
-const RedisStrategy = require('./RedisStrategy');
 const MemoryStrategy = require('./MemoryStrategy');
-const SQLStrategy = require('./SQLStrategy');
 
 module.exports = {
-  RedisStrategy,
-  MemoryStrategy,
-  SQLStrategy,
+  MemoryStrategy
 }
